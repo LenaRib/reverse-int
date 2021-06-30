c55f6199777576f106b548586a31217d3a4bb587
LenaRib <poddubnayaeo@gmail.com>
2021-06-30 11:41:51 +0200

    feat: add reverse number func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let module = Math.abs(n);
+    let str = module + '';
+    let array = str.split('');
+    let reverse_array = array.reverse();
+    let rexerse_str = reverse_array.join('');
+    let reverse_num = Number(rexerse_str);
+    return reverse_num;
 }
